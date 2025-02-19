cbbb5923d179c14c67272a22202f50d0362ffcc5
literandltx <romanskovronn@gmail.com>
2025-02-19 22:53:06 +0200

    refactor curve struct, optimize number of module modulo operation, move tests, add metric function

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,15 +1,17 @@
 // helpers.rs
 use num_bigint::BigInt;
 use num_traits::Zero;
-use rand::prelude::ThreadRng;
-use rand::Rng;
+use rand::{prelude::ThreadRng, Rng};
+use std::time::Instant;
 
 pub fn module(x: &BigInt, modulo: &BigInt) -> BigInt {
-    if *x < BigInt::zero() {
-        return modulo + x;
+    let result: BigInt = x % modulo;
+
+    if result < BigInt::zero() {
+        return modulo + result;
     }
 
-    x % modulo
+    result
 }
 
 pub fn generate_random_bigint(bit_length: usize) -> BigInt {
@@ -25,4 +27,28 @@ pub fn generate_random_bigint(bit_length: usize) -> BigInt {
     }
 
     result
+}
+
+fn measure_average_execution_time<F>(method_name: &str, method_to_run: F, iterations: usize)
+where
+    F: Fn() -> (),
+{
+    let mut total_duration_in_nano: u128 = 0u128;
+
+    for _ in 0..iterations {
+        let start_time: Instant = Instant::now();
+
+        method_to_run();
+
+        let end_time: Instant = Instant::now();
+        total_duration_in_nano += end_time.duration_since(start_time).as_nanos();
+    }
+
+    let avg_duration_in_nano: u128 = total_duration_in_nano / iterations as u128;
+    let avg_duration_in_millis: u128 = avg_duration_in_nano / 1_000_000;
+
+    println!("Operation called: {}", method_name);
+    println!("Average Execution time (ns): {}", avg_duration_in_nano);
+    println!("Average Execution time (ms): {}", avg_duration_in_millis);
+    println!();
 }
