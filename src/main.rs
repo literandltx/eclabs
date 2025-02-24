9533b12f51a8993a33c6da9bdc053d3807456221
serhii023 <serhii023@gmail.com>
2025-02-24 11:29:43 +0200

    add fast random point generation

--- src/main.rs
+++ src/main.rs
@@ -265,33 +265,22 @@ impl Curve {
         random_point
     }
 
-    fn generate_random_projective_point_p256(&self) -> ProjectivePoint {
-        let rand: BigInt = generate_random_bigint(128);
-
-        let left: BigInt = (rand.clone().pow(3u32) + &self.a * rand.clone() + &self.b) % &self.p;
-
-        // todo: impl sqrt(right) mod p
-        let sqrt: BigInt = left.clone();
-
-        let point: ProjectivePoint = ProjectivePoint::new(rand, sqrt, BigInt::one());
-
-        match self.verify_projective_point(&point) {
-            true => point,
-            false => self.generate_random_projective_point_p256()
-        }
-    }
-
     fn generate_random_projective_point_p256_fast(&self) -> ProjectivePoint {
         loop {
-            let x: BigInt = helpers::generate_random_bigint(128);
-            let n: BigInt = x.clone().pow(3u32) + &self.a * x.clone() + &self.b;
+            let x: BigInt = generate_random_bigint(256);
+            let right: BigInt = x.clone().pow(3u32) + &self.a * x.clone() + &self.b;
 
+            // right^((p - 1) / 2)
             let check_value: BigInt =
-                n.modpow(&((&self.p - BigInt::one()) / BigInt::from(2u32)), &self.p);
+                right.modpow(&((&self.p - BigInt::one()) / BigInt::from(2u32)), &self.p);
+
+            // check is there existing solutions
             if check_value.eq(&BigInt::one()) {
-                // check is there existing solutions
                 let k: BigInt = (&self.p - BigInt::from(3u32)) / BigInt::from(4u32);
-                let y: BigInt = n.modpow(&(k + BigInt::one()), &self.p);
+
+                // y = +right^(k + 1)
+                let y: BigInt = right.modpow(&(k + BigInt::one()), &self.p);
+
                 return ProjectivePoint::new(x, y, BigInt::one());
             }
         }
